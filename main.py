72724c4144656b8fa8458a41717b63d4a930f6ed
rushdi130 <rushdi130@gmail.com>
2022-09-07 17:34:48 +0100

    3rd commit

--- main.py
+++ main.py
@@ -21,11 +21,57 @@ while play_again == 'y':
 
     if guess1 == x:
         print ("Wtf... my integer was",x,",you won on the 1st guess. Well done you lucky yute O_O")
+        play_again = input("Do you want to play again? [y/n]")
 
     elif guess1 > x:
         while True:
             try:
                 guess2 = int(input("Your guess was too high buddy... Input your second guess:"))
+                if guess2 == x:
+                    print("Well done... my integer was", x, ",you won on the 2nd guess. HURRAH! :P")
+                    play_again = input("Do you want to play again? [y/n]")
+
+                elif guess2 > x:
+                    while True:
+                        try:
+                            guess3 = int(input("Your guess was too high buddy... Input your third guess:"))
+                            if guess3 == x:
+                                print("Well done... my integer was", x,
+                                      ",you won on the final 3rd guess. Which is a MEH")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+
+                            elif guess3 != x:
+                                print(
+                                    "I gave you 3 guesses and you still can't get it right YOU FAILURE! My integer was",
+                                    x, "lul")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+                        except ValueError:
+                            print("Your guess has to be an integer my guy... Try again")
+                        else:
+                            break
+
+                elif guess2 < x:
+                    while True:
+                        try:
+                            guess3 = int(input("Your guess was too low buddy... Input your third guess:"))
+                            if guess3 == x:
+                                print("Well done... my integer was", x,
+                                      ",you won on the final 3rd guess. Which is a MEH")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+
+                            elif guess3 != x:
+                                print(
+                                    "I gave you 3 guesses and you still can't get it right YOU FAILURE! My integer was",
+                                    x, "lul")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+                        except ValueError:
+                            print("Your guess has to be an integer my guy... Try again")
+                        else:
+                            break
             except ValueError:
                 print("Your guess has to be an integer my guy... Try again")
             else:
@@ -35,36 +81,52 @@ while play_again == 'y':
         while True:
             try:
                 guess2 = int(input("Your guess was too low buddy... Input your second guess:"))
+                if guess2 == x:
+                    print("Well done... my integer was", x, ",you won on the 2nd guess. HURRAH! :P")
+                    play_again = input("Do you want to play again? [y/n]")
+
+                elif guess2 > x:
+                    while True:
+                        try:
+                            guess3 = int(input("Your guess was too high buddy... Input your third guess:"))
+                            if guess3 == x:
+                                print("Well done... my integer was", x,
+                                      ",you won on the final 3rd guess. Which is a MEH")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+
+                            elif guess3 != x:
+                                print(
+                                    "I gave you 3 guesses and you still can't get it right YOU FAILURE! My integer was",
+                                    x, "lul")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+                        except ValueError:
+                            print("Your guess has to be an integer my guy... Try again")
+                        else:
+                            break
+
+                elif guess2 < x:
+                    while True:
+                        try:
+                            guess3 = int(input("Your guess was too low buddy... Input your third guess:"))
+                            if guess3 == x:
+                                print("Well done... my integer was", x,
+                                      ",you won on the final 3rd guess. Which is a MEH")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+
+                            elif guess3 != x:
+                                print(
+                                    "I gave you 3 guesses and you still can't get it right YOU FAILURE! My integer was",
+                                    x, "lul")
+                                play_again = input("Do you want to play again? [y/n]")
+                                break
+                        except ValueError:
+                            print("Your guess has to be an integer my guy... Try again")
+                        else:
+                            break
             except ValueError:
                 print("Your guess has to be an integer my guy... Try again")
             else:
                 break
-
-    if guess2 == x:
-        print ("Well done... my integer was",x,",you won on the 2nd guess. HURRAH! :P")
-
-    elif guess2 > x:
-        while True:
-            try:
-                guess3 = int(input("Your guess was too high buddy... Input your third guess:"))
-            except ValueError:
-                print("Your guess has to be an integer my guy... Try again")
-            else:
-                break
-
-    elif guess2 < x:
-        while True:
-            try:
-                guess3 = int(input("Your guess was too low buddy... Input your third guess:"))
-            except ValueError:
-                print("Your guess has to be an integer my guy... Try again")
-            else:
-                break
-
-    if guess3 == x:
-        print ("Well done... my integer was",x,",you won on the final 3rd guess. Which is a MEH")
-
-    elif guess3 != x:
-            print ("I gave you 3 guesses and you still can't get it right YOU FAILURE! My integer was",x,"lul")
-
-    play_again = input("Do you want to play again? [y/n]")
